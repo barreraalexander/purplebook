78d97856bf38c43c040e82fc27936363c1ebe9c2
Barrera <barreraalexander93@gmail.com>
2021-11-09 06:52:14 -0500

    moved auth routes to their proper blueprint

--- backend/server/blueprints/api/routes.py
+++ backend/server/blueprints/api/routes.py
@@ -1,10 +1,9 @@
-from server import db, bcrypt, login_manager
+from server import db, bcrypt
 from server.blueprints.graphql import RES_DICTS 
 from server.schemas.book import Book_DB, schema as BookSchema
 from server.schemas.user import User_DB, schema as UserSchema
 from server.models.user import Auth_Model
 from flask import Blueprint, request
-from flask_login import logout_user, login_user
 # from flask_login import login_user as authorize_user
 
 
@@ -32,36 +31,3 @@ def create_tables():
 
     return {'hey' : 'hi'}, 400
 
-@api.route('/authorize_user', methods=['POST'])
-def authorize_user():
-
-    query = ""
-    if request.method=='POST':
-        input_email = request.form.get('email')
-        input_pass = request.form.get('password')
-
-        query  = """
-            query auth {
-                user_by_email(email:"%s") {
-                    id
-                    email
-                    password
-                }
-            }
-        """ % (input_email)
-        
-        res = UserSchema.execute(query)
-
-        if res.errors:
-            print (res.errors)
-            return RES_DICTS['error']
-
-        mdict = res.data['user_by_email']
-        print (mdict)
-        auth_model = Auth_Model(mdict)
-        login_user(auth_model)
-        return RES_DICTS['good']
-
- 
-
-    return RES_DICTS['error']
--- backend/server/blueprints/auth/routes.py
+++ backend/server/blueprints/auth/routes.py
@@ -1,38 +1,58 @@
 from flask import Blueprint, request
+from flask_login import login_required
 from server.schemas.book import schema as BookSchema
 
+from server import db, bcrypt, login_manager
+from server.blueprints.graphql import RES_DICTS 
+from server.schemas.user import User_DB, schema as UserSchema
+from server.models.user import Auth_Model
+from flask import Blueprint, request
+from flask_login import logout_user, login_user
 
 auth = Blueprint("auth", __name__,
     url_prefix="/auth")
 
-@auth.route("/login", methods=['POST'])
-def authorize_login():
-    if request.method=="POST":
-        print ('posted')
-        return (request.data)
-        # return {'status' : 'good'}, 400
-    return {'status' : 'bad'}, 200
+
+@auth.route('/authorize_user', methods=['POST'])
+def authorize_user():
+    if request.method=='POST':
+        input_email = request.form.get('email')
+        input_pass = request.form.get('password')
+
+        query  = """
+            query auth {
+                user_by_email(email:"%s") {
+                    id
+                    email
+                    password
+                }
+            }
+        """ % (input_email)
+        
+        res = UserSchema.execute(query)
+
+        if res.errors:
+            print (res.errors)
+            return RES_DICTS['error']
+
+        mdict = res.data['user_by_email']
+        auth_model = Auth_Model(mdict)
+        login_user(auth_model)
+        return RES_DICTS['good']
+    return RES_DICTS['error']
 
 @auth.route("/register")
 def authorize_register():
-    return {'status' : 'good'}, 400
-    # pass
+    return RES_DICTS['error']
 
 
-# @api.route('/gql/<string:query>', methods=['POST', 'GET'])
-# def booker(_query):
-#     post_request = jsonify(request.data)
-#     # post_request = json.loads(request.data)
-#     # schema_data = schema.execute(post_request)
-#     # d
-#     schema_data = json.dump(schema.execute(post_request['query']).data)
+@auth.route("/logout")
+@login_required
+def logout():
+    try:
+        logout_user()
+        return RES_DICTS['good']
 
-#     return (schema_data)
-#     # return schema.execute()
-#     pass
-
-# # json looks like this for post request:
-
-# {
-#     "query" : "{ hello(age:99) }"
-# }
+    except Exception as err:
+        print (err)
+        return RES_DICTS['error']
